e2adcaf5f022edf82743c15ed21b99e2c1545272
jigarWala <jigar.wala@somaiya.edu>
2017-01-26 20:29:21 +0530

    sdvs

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 'use strict'
+
+const token = process.env.FB_PAGE_ACCESS_TOKEN
+const vtoken = process.env.FB_VERIFY_ACCESS_TOKEN
+
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
@@ -6,6 +10,7 @@ const app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
+// Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
 
 // Process application/json
@@ -18,11 +23,11 @@ app.get('/', function (req, res) {
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'verifyme') {
+    if (req.query['hub.verify_token'] === vtoken) {
         res.send(req.query['hub.challenge'])
     }
     else
-    res.send('Error, wrong token')
+    res.send('No sir')
 })
 
 // Spin up the server
